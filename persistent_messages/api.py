a5eb7882a733b92f523a6d64820e93c8bae7e04c
philomat <philomat@popkultur.net>
2010-10-01 18:12:12 +0200

    modified:   persistent_messages/api.py 	modified:   persistent_messages/templates/persistent_messages/message/includes/messages.html

--- persistent_messages/api.py
+++ persistent_messages/api.py
@@ -1,4 +1,5 @@
 from persistent_messages import notify
+from persistent_messages import constants 
 
 def add_message(request, level, message, extra_tags='', fail_silently=False, subject='', user=None, email=False, from_user=None, expires=None, close_timeout=None):
     """
@@ -6,3 +7,21 @@ def add_message(request, level, message, extra_tags='', fail_silently=False, sub
     if email:
         notify.email(level, message, extra_tags, subject, user, from_user)
     return request._messages.add(level, message, extra_tags, subject, user, from_user, expires, close_timeout)
+
+def info(request, message, extra_tags='', fail_silently=False, subject='', user=None, email=False, from_user=None, expires=None, close_timeout=None):
+    """
+    """
+    level = constants.INFO
+    return add_message(request, level, message, extra_tags, fail_silently, subject, user, email, from_user, expires, close_timeout)
+
+def warning(request, message, extra_tags='', fail_silently=False, subject='', user=None, email=False, from_user=None, expires=None, close_timeout=None):
+    """
+    """
+    level = constants.WARNING
+    return add_message(request, level, message, extra_tags, fail_silently, subject, user, email, from_user, expires, close_timeout)
+
+def debug(request, message, extra_tags='', fail_silently=False, subject='', user=None, email=False, from_user=None, expires=None, close_timeout=None):
+    """
+    """
+    level = constants.DEBUG
+    return add_message(request, level, message, extra_tags, fail_silently, subject, user, email, from_user, expires, close_timeout)
