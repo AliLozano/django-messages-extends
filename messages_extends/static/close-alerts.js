d043f2b5a9012480cb49473f818390c5c1aefcf2
Felipe Prenholato <philipe.rp@gmail.com>
2012-08-27 10:54:08 -0300

    jQuery js need to be executed after document is ready.

--- messages_extends/static/close-alerts.js
+++ messages_extends/static/close-alerts.js
@@ -1,6 +1,9 @@
-$("a.close[close-href]").click(function (e) {
-        e.preventDefault();
-        $.post($(this).attr("close-href"), "", function () {
-        });
-    }
-);
+$(function() {
+    $("a.close[close-href]").click(function (e) {
+            e.preventDefault();
+            $.post($(this).attr("close-href"), "", function () {
+            });
+        }
+    );
+});
+
