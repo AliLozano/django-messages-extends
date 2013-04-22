28b4300d4314078e089a7abd084842baea903d85
Steven Klass <sklass@7stalks.com>
2013-04-22 11:28:45 -0700

    Added in tests.  Added in shortcut methods.  Corrected some of the text.  Added in licensing and credits.

--- messages_extends/__init__.py
+++ messages_extends/__init__.py
@@ -17,3 +17,52 @@ def add_message(request, level, message, extra_tags='', fail_silently=False, *ar
 messages.add_message = add_message
 
 
+def persistant_debug(request, message, extra_tags='', fail_silently=False, *args, **kwargs):
+    """
+    Adds a persistant message with the ``DEBUG`` level.
+    """
+    add_message(request, DEBUG_PERSISTENT, message, extra_tags=extra_tags,
+                fail_silently=fail_silently, *args, **kwargs)
+
+messages.persistant_debug = persistant_debug
+
+
+def persistant_info(request, message, extra_tags='', fail_silently=False, *args, **kwargs):
+    """
+    Adds a persistant message with the ``INFO`` level.
+    """
+    add_message(request, INFO_PERSISTENT, message, extra_tags=extra_tags,
+                fail_silently=fail_silently, *args, **kwargs)
+
+messages.persistant_info = persistant_info
+
+
+def persistant_success(request, message, extra_tags='', fail_silently=False, *args, **kwargs):
+    """
+    Adds a persistant message with the ``SUCCESS`` level.
+    """
+    add_message(request, SUCCESS_PERSISTENT, message, extra_tags=extra_tags,
+                fail_silently=fail_silently, *args, **kwargs)
+
+messages.persistant_success = persistant_success
+
+
+def persistant_warning(request, message, extra_tags='', fail_silently=False, *args, **kwargs):
+    """
+    Adds a persistant message with the ``WARNING`` level.
+    """
+    add_message(request, WARNING_PERSISTENT, message, extra_tags=extra_tags,
+                fail_silently=fail_silently, *args, **kwargs)
+
+messages.persistant_warning = persistant_warning
+
+
+def persistant_error(request, message, extra_tags='', fail_silently=False, *args, **kwargs):
+    """
+    Adds a persistant message with the ``ERROR`` level.
+    """
+    add_message(request, ERROR_PERSISTENT, message, extra_tags=extra_tags,
+                fail_silently=fail_silently, *args, **kwargs)
+
+messages.persistant_error = persistant_error
+
