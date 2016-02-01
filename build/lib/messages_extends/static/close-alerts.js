ad7bee06b5de6d951ff60d213b0db9d29b930eb3
Ali Lozano <alilozanoc@gmail.com>
2016-02-01 17:19:58 -0500

    deleted build

--- build/lib/messages_extends/static/close-alerts.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $("a.close[close-href]").click(function (e) {
-            e.preventDefault();
-            $.post($(this).attr("close-href"), "", function () {
-            });
-        }
-    );
-});
-
